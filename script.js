55eef53ea59567072fae38eeba25b5994b9586a1
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-21 00:15:32 +0500

    i added  myteam page

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ let lname='Murodjonov'
 let age=20
 let study='TATU Unversity'
 let info='i am not married'
-console.log('mening ismim '+ fname+' '+lname +' Mening yoshim '+age +'men '+study+" o'qiyman"+' '+ info);
+console.log('mening ismim '+ fname+' '+lname +' Mening yoshim '+age +' men '+study+" o'qiyman"+' '+ info);
+console.log( 2400/8); //math function
+
