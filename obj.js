6fc866da7c9d3d66c287405dd8a077b2e474d897
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-25 12:52:58 +0500

    perfect object info

---
+++ obj.js
@@ -0,0 +1,16 @@
+
+let prom0 = prompt("isminingizni kiritin");
+let prom1= prompt("familyani kiriting");
+let prom3= prompt("yoshingizni kiriting");
+let prom4= prompt("yoqtirgan son kiriting");
+ obj= {
+    myname: prom0,
+    mysurname: prom1,
+    age: prom3,
+    myfavnumber:prom4,
+    
+}
+console.log( " siznig ismingiz  "+ obj.myname + " va familyangiz "+obj.mysurname+ " yoshingiz "+obj.age+" yoqtirgan soningiz "+obj.myfavnumber)
+
+
+
