8c0ecb930ee2330894a6003be5a0dfe1b9702310
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-28 13:18:52 +0500

    juft sonlarni kiriting

---
+++ knowledge/homework.js
@@ -0,0 +1,15 @@
+
+for (let j = 0; j <= 10; j++) {
+    if (j % 2 != 0) {
+        j++;
+        console.log(j);
+    }
+} 
+
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+// N - 3, son 4 ga teng bulib qolsa loop tuxtasin...
---
+++ knowledge/script.js
@@ -0,0 +1,99 @@
+// let prom = Number(prompt("yosh kiritin"));
+// switch(prom){
+//     case 20:
+//     alert("yoshingiz kichik");
+//     break;
+//     default:
+//         alert("yoshingiz katta")
+// }
+
+// let num=0;
+// while(num < 3) {
+//     let prom = Number(prompt("your code"));
+//     switch(prom){
+//         case 2022:
+//             num=3
+//         alert("open");
+//         break;
+//         default:
+//             alert("Error");
+//     }
+//     num ++;
+// }
+
+
+
+// hafta kuni
+
+    // let prom = Number(prompt("hafta kuni"));
+    // switch(prom){
+    //     case 1:
+    //     alert("Monday");
+    //     break;
+    //      case 2:
+    //     alert("tuesday");
+    //     break;
+    //     case 3:
+    //     alert("wendesday");
+    //         break;
+    //     case 4:
+    //     alert("thursday");
+    //     break;
+    //     case 5:
+    //     alert("friday");
+    //     break;
+    //     case 6:
+    //     alert("saturday");
+    //     break;
+    //     case 7:
+    //     alert("sunday");
+    //     break;
+    //     default:
+    //         alert("Error");
+    // }
+    // if(prom===7){
+    //     alert("hafta tugadi")
+    // }
+    // else{
+    //     alert("ertaga o'qishga borishing kerak")
+    // }
+    
+
+
+//     let num = 0;
+// while( num < 3 ) {
+//     switch(num){
+//         case 1
+//         console.log( "*" );
+//         break;
+//         case 2
+//         console.log( "**" )
+//         break;
+//         case 3
+//         console.log( "***" )
+//         break;
+//     }
+   
+//     num ++;
+// }
+
+
+// let i = 1, n = 5;
+// while (i <= n) {
+//     console.log(i);
+//     i += 1;
+// }
+
+let sum = 0;
+let number = 0;
+
+
+while(number >= 0){
+    sum += number;
+    number = parseInt(prompt('Enter a number: '));
+} 
+
+console.log(`The sum is ${sum}.`);
+
+
+
