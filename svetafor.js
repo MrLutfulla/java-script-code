e54d9e7ba977f33e2f5ab34b52f20c16446782db
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-28 15:16:10 +0500

    svetafor work

---
+++ svetafor.js
@@ -0,0 +1,10 @@
+let color = prompt("Sveatafor rangini kiriting.")
+if (color === "yashil") {
+    alert("qizil")
+} else if (color === "sariq") {
+    alert("qizil yoki yashil")
+} else if (color === "qizil") {
+    alert("sariq")
+} else if(color === " "){
+    alert("Siz noto'g'ri rang kiritgingiz.")
+}
