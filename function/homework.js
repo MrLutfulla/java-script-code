b12506d0946184ddb565dbf4ee939aad15528a92
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-12-07 14:57:32 +0500

    fail code

---
+++ function/homework.js
@@ -0,0 +1,82 @@
+
+let names = prompt("enter your name");
+
+let surnames = prompt("enter your last name");
+
+let ages = +prompt("enter your age")
+
+function setinfo( name, surname, age ){
+   
+	infos.student1[name]= names
+    infos.student1[surname]= surnames
+	infos.student1[age]= ages
+
+}
+let infos = {
+
+    student1: {
+    },
+
+    student2: {
+    },
+
+    student3: {
+		
+
+    }
+
+}
+
+
+
+
+
+setinfo()
+console.log(infos)
+
+// infos[ "student" + i] = setinfo()
+
+
+// let infos = {
+	
+// }
+// // for(let i = 1; i <= 3; i++){
+//   let name = prompt("ismingizni kiriting")
+//     let surname = prompt("ismingizni kiriting")
+//     let age = prompt("ismingizni kiriting")
+//     setInfo(name, surname, age)
+// // }
+
+
+// function setInfo(name, surname, age, i = "1"){
+//     let key = "student" + i;
+//     infos.key[name] = name;
+//     infos.key[surname] = surname;
+//     infos.key[age] = age;
+// }
+
+
+  
+// console.log(infos)
+
+// let names = prompt("ismingizni kiriting")
+// let surname = prompt("familyani kirit")
+// let age= prompt("familyani kirit")
+
+
+
+// let infos = {
+// 	student1: {	
+// 	},
+// 	studet2: {
+// 		newname:
+		
+// 	},
+// 	genres: [],
+// 	private: false,
+// }
+
+// infos.student1[name]= names
+// infos.student2.newname= surname
+// infos.student2.age = age
+// console.log(infos.student1)
---
+++ function/javascript.js
@@ -0,0 +1,123 @@
+//Declaration, Expression, Arrow Functions
+//Scopes
+
+//Function Declaration
+
+// function sayHi() {
+// 	console.log("Salom")
+// }
+
+// function sayBye() {
+// 	console.log("Hayr")
+// }
+
+// function showMessage() {
+// 	console.log("Akbar salom berishga tayyorlanyabdi...")
+// 	sayHi()
+// 	console.log("Akbar gaplashyabdi...")
+// 	console.log("Akbar hayrlashishga tayyorlanyabdi...")
+// 	sayBye()
+// }
+
+// showMessage()
+
+// Function Expression
+
+// sayHi()
+
+// let sayHi = function () {
+// 	console.log("Hello")
+// }
+
+// let say = sayHi
+
+//Function Declaration
+
+// sayBye()
+
+// function sayBye() {
+// 	console.log("Bye")
+// }
+
+//Arrow Function
+
+// let sayHello = () => {
+// 	console.log("Hello")
+// }
+
+// sayHello()
+
+// return
+
+// function teaching() {
+// 	console.log("Run...")
+// }
+
+// function run() {
+// 	console.log("Run...")
+// 	return 5
+// }
+
+// let card = run() //teaching()
+
+// console.log(card)
+
+//Local Scope
+let num = 6
+
+function showMessage() {
+	num = 125
+	console.log(num)
+}
+
+showMessage()
+console.log(num)
+
+//Arguments
+
+function getInfo(name = "Alex", surname = "Smith", age = 15) {
+	console.log(name, surname, age)
+}
+
+getInfo("Akbar", "Vayisov")
+
+//Homework
+// 1. Ism, Familiya, Age (prompt)
+// 2. setInfo funksiya - bu funksiya 3ta promptdan keladigan qiymatlar object ko'rinishida qaytarsin
+// 3. infos o'zgaruvchiga object saqlaymiz undagi students keyinda objectlar saqlansin
+
+// setInfo(name, surname, age)
+
+// {
+// 	name:
+// 	surname:
+// 	age:
+// }
+
+let infos = {
+	student1: {
+		name:
+	},
+	student2: {
+        name:
+
+	},
+	student3: {
+        name:
+	}
+}
+
+
+function setInfo(name, surname, age, i){
+    infos["student" + i][name] = name;
+    infos["student" + i][surname] = surname;
+    infos["student" + i][age] = age;
+}
+
+for(let i = 1; i <= 3; i++){
+    let name =prompt("ismingizni kiriting")
+    let surname =prompt("ismingizni kiriting")
+    let age =prompt("ismingizni kiriting")
+    setInfo(name, surname, age)
+}
+console.log(infos)
