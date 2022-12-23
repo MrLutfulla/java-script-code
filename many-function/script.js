dc974b577e4e4b40e98c13aa502f16250c4ea566
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-12-23 15:24:43 +0500

    homework

--- many-function/script.js
+++ many-function/script.js
@@ -3,61 +3,61 @@ const sheeps = ['echki', 'quy', 'quzi'];
 
 const cloneSheeps = sheeps.slice();
 cloneSheeps[0] = 'key'
-// console.log(cloneSheeps)
+console.log(cloneSheeps)
 
 // Method2================================
-// let numbers = [45, 4, 9, -16, 25];
+let numbers = [45, 4, 9, -16, 25];
 
-// const clonenumbers = numbers.filter(myFunction);
+const clonenumbers = numbers.filter(myFunction);
 
-// function myFunction(value, array) {
-//         return value > 0 || 0 >= value;
-//       }
-//       clonenumbers[0] = '3'
-// console.log(clonenumbers)
+function myFunction(value, array) {
+        return value > 0 || 0 >= value;
+      }
+      clonenumbers[0] = '3'
+console.log(clonenumbers)
 
 //   mathod3===============================
 
-// const array = ['aka', 'uka', 'opa', 'ona', 'dada']//[45, 4, 9, -16, 25]// 
-// const clonearray = [...array];
-// clonearray[0] = 'tog\'a';
-// console.log(clonearray)
-// console.log(array)
+const array = ['aka', 'uka', 'opa', 'ona', 'dada']//[45, 4, 9, -16, 25]// 
+const clonearray = [...array];
+clonearray[0] = 'tog\'a';
+console.log(clonearray)
+console.log(array)
 
 //method4====================================
 const nums = [1, 2, 3, 4, 5];
 
-// const clonenums = Array.from(nums);
-// clonenums[0] = 21;
-// console.log(clonenums)
-// console.log(nums)
+const clonenums = Array.from(nums);
+clonenums[0] = 21;
+console.log(clonenums)
+console.log(nums)
 //method5=====================================
-// let a = [[1], [2], [3]];
-// const b = (function fun(ar){
-//  return ar.map(el=>Array.isArray(el)&&fun(el)||el) 
-// })(a);
+let a = [[1], [2], [3]];
+const b = (function fun(ar){
+ return ar.map(el=>Array.isArray(el)&&fun(el)||el) 
+})(a);
 
-// a[0][0]='a';
-// console.log('a',a);
-// console.log('b',b);  
+a[0][0]='a';
+console.log('a',a);
+console.log('b',b);  
 // mathod5 function============================
 
 
-// function deepCopyArray(arr) {
-//     return arr.map(item => {
-//       if (Array.isArray(item)) {
-//         return deepCopyArray(item);
-//       } else if (typeof item === "object") {
-//         return Object.assign({}, item);
-//       }
-//       else return item;
-//     });
-//   }
+function deepCopyArray(arr) {
+    return arr.map(item => {
+      if (Array.isArray(item)) {
+        return deepCopyArray(item);
+      } else if (typeof item === "object") {
+        return Object.assign({}, item);
+      }
+      else return item;
+    });
+  }
   
-//   let newArray = deepCopyArray(nums);
+  let newArray = deepCopyArray(nums);
   
-//   newArray[1] = 0;
-//   let clone = console.log(newArray, nums);
+  newArray[1] = 0;
+  let clone = console.log(newArray, nums);
 // =================================================
 const numbers = [45, 4, 9, 16, 25];
 
